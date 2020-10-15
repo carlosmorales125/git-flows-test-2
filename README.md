92a0c158aaf38eb103a94966f69ad413123e0077
Carlos Morales <carlos.rey.morales@gmail.com>
2020-10-15 14:18:42 -0400

    still still working f3

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # git-flows-test-2
 
 ## F3
-still working on F3
+- still working on F3
+- still still working on F3
--- f3.js
+++ f3.js
@@ -1,4 +1,5 @@
 const f3 = 'feature 3';
 const stillWorking = 'still working f3';
+const lookAtMe = 'still still working f3';
 
 export default f3;
